0879ae3e643136254e05b1791f1841c49f5b3d42
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-26 11:47:19 +0800

    颜色

--- src/ColoursEx.java
+++ src/ColoursEx.java
@@ -6,7 +6,7 @@ import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-public class StrokeFillEx extends Application {
+public class ColoursEx extends Application {
 
     @Override
     public void start(Stage stage) {
@@ -20,24 +20,36 @@ public class StrokeFillEx extends Application {
 
         Canvas canvas = new Canvas(300, 300);
         GraphicsContext gc = canvas.getGraphicsContext2D();
-        doDrawing(gc);
+        drawShapes(gc);
 
         root.getChildren().add(canvas);
 
-        Scene scene = new Scene(root, 300, 250, Color.WHITESMOKE);
+        Scene scene = new Scene(root, 280, 200, Color.WHITESMOKE);
 
-        stage.setTitle("Stroke and fill");
+        stage.setTitle("Colours");
         stage.setScene(scene);
         stage.show();
     }
 
-    private void doDrawing(GraphicsContext gc) {
+    private void drawShapes(GraphicsContext gc) {
 
-        gc.setStroke(Color.FORESTGREEN.brighter());
-        gc.setLineWidth(5);
-        gc.strokeOval(30, 30, 80, 80);
-        gc.setFill(Color.FORESTGREEN);
-        gc.fillOval(130, 30, 80, 80);
+        gc.setFill(Color.CADETBLUE);
+        gc.fillOval(30, 30, 50, 50);
+
+        gc.setFill(Color.DARKRED);
+        gc.fillOval(110, 30, 50, 50);
+
+        gc.setFill(Color.STEELBLUE);
+        gc.fillOval(190, 30, 50, 50);
+
+        gc.setFill(Color.BURLYWOOD);
+        gc.fillOval(30, 110, 50, 50);
+
+        gc.setFill(Color.LIGHTSEAGREEN);
+        gc.fillOval(110, 110, 50, 50);
+
+        gc.setFill(Color.CHOCOLATE);
+        gc.fillOval(190, 110, 50, 50);
     }
 
     public static void main(String[] args) {
